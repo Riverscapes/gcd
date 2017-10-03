a0c3ffdc988dbe490618757002ca67dc5262e9b6
Philip Bailey <philip@northarrowresearch.com>
2017-10-03 12:29:01 -0700

    initial commit

---
+++ GCDAddIn/ucDockableWindow.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GCDAddIn
+{
+    /// <summary>
+    /// Designer class of the dockable window add-in. It contains user interfaces that
+    /// make up the dockable window.
+    /// </summary>
+    public partial class ucDockableWindow : UserControl
+    {
+        public ucDockableWindow(object hook)
+        {
+            InitializeComponent();
+            this.Hook = hook;
+        }
+
+        /// <summary>
+        /// Host object of the dockable window
+        /// </summary>
+        private object Hook
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Implementation class of the dockable window add-in. It is responsible for 
+        /// creating and disposing the user interface class of the dockable window.
+        /// </summary>
+        public class AddinImpl : ESRI.ArcGIS.Desktop.AddIns.DockableWindow
+        {
+            private ucDockableWindow m_windowUI;
+
+            public AddinImpl()
+            {
+            }
+
+            protected override IntPtr OnCreateChild()
+            {
+                m_windowUI = new ucDockableWindow(this.Hook);
+                return m_windowUI.Handle;
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                if (m_windowUI != null)
+                    m_windowUI.Dispose(disposing);
+
+                base.Dispose(disposing);
+            }
+
+        }
+    }
+}
