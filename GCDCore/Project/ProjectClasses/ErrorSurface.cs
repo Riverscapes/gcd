9ee74c483872193f44d6cfd24ec3a7a77e1e3068
Philip Bailey <philip@northarrowresearch.com>
2017-11-20 10:12:20 -0800

    error surface serialization complete

--- GCDCore/Project/ProjectClasses/ErrorSurface.cs
+++ GCDCore/Project/ProjectClasses/ErrorSurface.cs
@@ -6,22 +6,18 @@ namespace GCDCore.Project
 {
     public class ErrorSurface : GCDProjectItem
     {
-        public readonly string ErrorSurfaceType;
         public readonly ProjectRaster Raster;
         public readonly DEMSurvey DEM;
 
-        public Dictionary<string, ErrorFISProperties> FISProperties { get; internal set; }
-        public Dictionary<string, ErrorUniformProperties> UniformProperties { get; internal set; }
+        public readonly Dictionary<string, ErrorSurfaceProperty> ErrorProperties;
 
-        public ErrorSurface(string name, string sType, FileInfo rasterPath, DEMSurvey dem)
+        public ErrorSurface(string name, FileInfo rasterPath, DEMSurvey dem, Dictionary<string, ErrorSurfaceProperty> errProperties)
             : base(name)
         {
-            ErrorSurfaceType = sType;
             Raster = new ProjectRaster(rasterPath);
             DEM = dem;
 
-            FISProperties = new Dictionary<string, ErrorFISProperties>();
-            UniformProperties = new Dictionary<string, ErrorUniformProperties>();
+            ErrorProperties = errProperties;
         }
 
         public void Serialize(XmlDocument xmlDoc, XmlNode nodParent)
@@ -29,19 +25,28 @@ namespace GCDCore.Project
             XmlNode nodError = nodParent.AppendChild(xmlDoc.CreateElement("ErrorSurface"));
             nodError.AppendChild(xmlDoc.CreateElement("Name")).InnerText = Name;
             nodError.AppendChild(xmlDoc.CreateElement("Path")).InnerText = ProjectManagerBase.GetRelativePath(Raster.RasterPath);
+
+            XmlNode nodProperties = nodError.AppendChild(xmlDoc.CreateElement("ErrorSurfaceProperties"));
+            foreach (ErrorSurfaceProperty props in ErrorProperties.Values)
+            {
+                XmlNode nodProperty = nodProperties.AppendChild(xmlDoc.CreateElement("ErrorSurfaceProperty"));
+                props.Serialize(xmlDoc, nodProperty);
+            }
         }
 
         public static ErrorSurface Deserialize(XmlNode nodError, DEMSurvey dem)
         {
             string name = nodError.SelectSingleNode("Name").InnerText;
-            string type = nodError.SelectSingleNode("Type").InnerText;
             FileInfo path = ProjectManagerBase.GetAbsolutePath(nodError.SelectSingleNode("Path").InnerText);
 
-            ErrorSurface err = new ErrorSurface(name, type, path, dem);
-
-            // TODO: load error properties
-
-            return err;
+            Dictionary<string, ErrorSurfaceProperty> properties = new Dictionary<string, ErrorSurfaceProperty>();
+            foreach (XmlNode nodProperty in nodError.SelectNodes("ErrorSurfaceProperties/ErrorSurfaceProperty"))
+            {
+                ErrorSurfaceProperty prop = ErrorSurfaceProperty.Deserialize(nodProperty, dem);
+                properties[prop.Name] = prop;
+            }
+            
+            return new ErrorSurface(name, path, dem, properties); ;
         }
     }
 }
---
+++ GCDCore/Project/ProjectClasses/ErrorSurfaceProperty.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace GCDCore.Project
+{
+    public class ErrorSurfaceProperty : GCDProjectItem
+    {
+        public readonly double? UniformValue;
+        public readonly AssocSurface AssociatedSurface;
+
+        public readonly FileInfo FISRuleFile;
+        public readonly Dictionary<string, AssocSurface> FISInputs;
+
+        public ErrorSurfaceProperty(string name, double uniformValue) : base(name)
+        {
+            UniformValue = new double?(uniformValue);
+        }
+
+        public ErrorSurfaceProperty(string name, AssocSurface assoc) : base(name)
+        {
+            AssociatedSurface = assoc;
+        }
+
+        public ErrorSurfaceProperty(string name, FileInfo fisRuleFile, Dictionary<string, AssocSurface> fisInputs) : base(name)
+        {
+            FISRuleFile = fisRuleFile;
+            FISInputs = fisInputs;
+        }
+
+        public void Serialize(XmlDocument xmlDoc, XmlNode nodParent)
+        {
+            nodParent.AppendChild(xmlDoc.CreateElement("Name")).InnerText = Name;
+
+            if (UniformValue.HasValue)
+                nodParent.AppendChild(xmlDoc.CreateElement("UniformValue")).InnerText = UniformValue.ToString();
+
+            if (AssociatedSurface != null)
+                nodParent.AppendChild(xmlDoc.CreateElement("AssociatedSurface")).InnerText = AssociatedSurface.Name;
+
+            if (FISRuleFile is FileInfo)
+                nodParent.AppendChild(xmlDoc.CreateElement("FISRuleFile")).InnerText = ProjectManagerBase.GetRelativePath(FISRuleFile);
+
+            if (FISInputs != null)
+            {
+                XmlNode nodInputs = nodParent.AppendChild(xmlDoc.CreateElement("FISInputs"));
+                foreach (KeyValuePair<string, AssocSurface> input in FISInputs)
+                {
+                    XmlNode nodInput = nodInputs.AppendChild(xmlDoc.CreateElement("Input"));
+                    nodInput.AppendChild(xmlDoc.CreateElement("Name")).InnerText = input.Key;
+                    nodInput.AppendChild(xmlDoc.CreateElement("AssociatedSurface")).InnerText = input.Value.Name;
+                }
+            }
+        }
+
+        public static ErrorSurfaceProperty Deserialize(XmlNode nodProperty, DEMSurvey dem)
+        {
+            ErrorSurfaceProperty result = null;
+
+            string name = nodProperty.SelectSingleNode("Name").InnerText;
+
+            XmlNode nodUni = nodProperty.SelectSingleNode("Uniform");
+            XmlNode nodAss = nodProperty.SelectSingleNode("AssociatedSurface");
+            XmlNode nodFIS = nodProperty.SelectSingleNode("FISRuleFile");
+
+            if (nodUni is XmlNode)
+            {
+                double value = double.Parse(nodUni.InnerText);
+                result = new ErrorSurfaceProperty(name, value);
+            }
+            else if (nodAss is XmlNode)
+            {
+                AssocSurface assoc = dem.AssocSurfaces[nodAss.InnerText];
+                result = new ErrorSurfaceProperty(name, assoc);
+            }
+            else if (nodFIS is XmlNode)
+            {
+                FileInfo fisRuleFile = ProjectManagerBase.GetAbsolutePath(nodFIS.SelectSingleNode("FISRuleFile").InnerText);
+                Dictionary<string, AssocSurface> inputs = new Dictionary<string, AssocSurface>();
+
+                foreach (XmlNode nodInput in nodFIS.SelectNodes("Inputs/Input"))
+                    inputs[nodInput.SelectSingleNode("Name").InnerText] = dem.AssocSurfaces[nodInput.SelectSingleNode("AssociatedSurface").InnerText];
+
+                result = new ErrorSurfaceProperty(name, fisRuleFile, inputs);
+            }
+
+            return result;
+        }
+    }
+}
