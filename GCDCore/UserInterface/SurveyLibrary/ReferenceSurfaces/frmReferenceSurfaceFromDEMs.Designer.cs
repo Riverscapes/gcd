119edfc76ba1ce97b54238d6a133686b917218e1
Philip Bailey <philip@northarrowresearch.com>
2018-02-08 16:08:51 -0800

    reference surface from DEMs UI

--- GCDCore/UserInterface/SurveyLibrary/ReferenceSurfaces/frmReferenceSurfaceFromDEMs.Designer.cs
+++ GCDCore/UserInterface/SurveyLibrary/ReferenceSurfaces/frmReferenceSurfaceFromDEMs.Designer.cs
@@ -33,9 +33,9 @@
             this.cmdOK = new System.Windows.Forms.Button();
             this.cmdHelp = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.lstDEMs = new System.Windows.Forms.CheckedListBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -64,6 +64,7 @@
             this.cmdOK.TabIndex = 7;
             this.cmdOK.Text = "Save";
             this.cmdOK.UseVisualStyleBackColor = true;
+            this.cmdOK.Click += new System.EventHandler(this.cmdOK_Click);
             // 
             // cmdHelp
             // 
@@ -84,15 +85,16 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Name";
             // 
-            // textBox1
+            // txtName
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.txtName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(83, 13);
-            this.textBox1.MaxLength = 255;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(316, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtName.Location = new System.Drawing.Point(83, 13);
+            this.txtName.MaxLength = 255;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(316, 20);
+            this.txtName.TabIndex = 1;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
             // 
             // label2
             // 
@@ -105,13 +107,13 @@
             // 
             // textBox2
             // 
-            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(83, 74);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(316, 20);
-            this.textBox2.TabIndex = 5;
+            this.txtPath.Location = new System.Drawing.Point(83, 74);
+            this.txtPath.Name = "textBox2";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(316, 20);
+            this.txtPath.TabIndex = 5;
             // 
             // groupBox1
             // 
@@ -166,9 +168,9 @@
             this.Controls.Add(this.cboMethod);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtPath);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtName);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cmdHelp);
             this.Controls.Add(this.cmdOK);
@@ -190,9 +192,9 @@
         private System.Windows.Forms.Button cmdOK;
         private System.Windows.Forms.Button cmdHelp;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtPath;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.CheckedListBox lstDEMs;
         private System.Windows.Forms.Label label3;
--- GCDCore/UserInterface/SurveyLibrary/ReferenceSurfaces/frmReferenceSurfaceFromDEMs.cs
+++ GCDCore/UserInterface/SurveyLibrary/ReferenceSurfaces/frmReferenceSurfaceFromDEMs.cs
@@ -25,9 +25,109 @@ namespace GCDCore.UserInterface.SurveyLibrary.ReferenceSurfaces
             DEMSurveys = new naru.ui.SortableBindingList<GCDCore.Project.DEMSurvey>(GCDCore.Project.ProjectManager.Project.DEMSurveys.Values.ToList<GCDCore.Project.DEMSurvey>());
             lstDEMs.DataSource = DEMSurveys;
 
+            foreach (GCDConsoleLib.RasterOperators.MultiMathOpType val in Enum.GetValues(typeof(GCDConsoleLib.RasterOperators.MultiMathOpType)))
+            {
+                if (val.ToString().ToLower().Contains("standarddeviation"))
+                    cboMethod.Items.Add(new naru.db.NamedObject((long)val, "Standard Deviation"));
+                else
+                    cboMethod.Items.Add(new naru.db.NamedObject((long)val, val.ToString()));
+            }
+            cboMethod.SelectedIndex = 0;
+
             // ensure all DEMs are checked by default
             for (int i = 0; i < lstDEMs.Items.Count; i++)
                 lstDEMs.SetItemChecked(i, true);
+        }
+
+        private void cmdOK_Click(object sender, EventArgs e)
+        {
+            if (!ValidateForm())
+            {
+                this.DialogResult = DialogResult.None;
+                return;
+            }
+
+            System.IO.FileInfo fiOutput = GCDCore.Project.ProjectManager.Project.GetAbsolutePath(txtPath.Text); 
+
+            List<GCDConsoleLib.Raster> rInputs = new List<GCDConsoleLib.Raster>();
+            foreach(GCDCore.Project.DEMSurvey dem in lstDEMs.CheckedItems)
+            {
+                rInputs.Add(dem.Raster);
+            }
+
+            try
+            {
+                Cursor = Cursors.WaitCursor;
+
+                switch (((GCDConsoleLib.RasterOperators.MultiMathOpType)((naru.db.NamedObject)cboMethod.SelectedItem).ID))
+                {
+                    case GCDConsoleLib.RasterOperators.MultiMathOpType.Addition:
+                        GCDConsoleLib.RasterOperators.MultiAdd(rInputs, fiOutput);
+                        break;
+
+                    case GCDConsoleLib.RasterOperators.MultiMathOpType.Maximum:
+                        GCDConsoleLib.RasterOperators.Maximum(rInputs, fiOutput);
+                        break;
+
+                    case GCDConsoleLib.RasterOperators.MultiMathOpType.Mean:
+                        GCDConsoleLib.RasterOperators.Mean(rInputs, fiOutput);
+                        break;
+
+                    case GCDConsoleLib.RasterOperators.MultiMathOpType.Minimum:
+                        GCDConsoleLib.RasterOperators.Minimum(rInputs, fiOutput);
+                        break;
+
+                    case GCDConsoleLib.RasterOperators.MultiMathOpType.StandardDeviation:
+                        GCDConsoleLib.RasterOperators.StandardDeviation(rInputs, fiOutput);
+                        break;
+
+                    default:
+                        throw new Exception("Unhandled math operation type " + cboMethod.Text);
+                }
+
+                Cursor = Cursors.Default;
+                MessageBox.Show("Reference surface generated successfully.", Properties.Resources.ApplicationNameLong, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch(Exception ex)
+            {
+                naru.error.ExceptionUI.HandleException(ex, "Error generating reference surface from DEM surveys.");
+                this.DialogResult = DialogResult.None;
+            }
+        }
+
+        private bool ValidateForm()
+        {
+            // Sanity check to avoid empty names
+            txtName.Text.Trim();
+
+            if (string.IsNullOrEmpty(txtName.Text))
+            {
+                MessageBox.Show("You must provide a name for the reference surface.", "Missing Name", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtName.Select();
+                return false;
+            }
+
+            if (!GCDCore.Project.ProjectManager.Project.IsReferenceSurfaceNameUnique(txtName.Text, null))
+            {
+                MessageBox.Show("The GCD project already contains a reference surface with this name. Please choose a unique name for the reference surface.", "Name Already Exists", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtName.Select();
+                return false;
+            }
+
+            if (lstDEMs.CheckedItems.Count < 2)
+            {
+                MessageBox.Show("You must select at least two DEM surveys to generate a reference surface.", "Insufficient DEM Surveys", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                lstDEMs.Select();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+
         }
     }
 }
