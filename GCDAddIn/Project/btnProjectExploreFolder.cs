06c085f3cbdc41272150abeacfd2a8e1303cc400
Philip Bailey <philip@northarrowresearch.com>
2018-05-07 15:39:42 -0700

    Explore project folder from addin fixes #267

---
+++ GCDAddIn/Project/btnProjectExploreFolder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GCDAddIn.Project
+{
+    class btnProjectExploreFolder : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        protected override void OnClick()
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(GCDCore.Project.ProjectManager.Project.Folder.FullName);
+            }
+            catch (Exception ex)
+            {
+                GCDCore.GCDException.HandleException(ex);
+            }
+        }
+
+        protected override void OnUpdate()
+        {
+            Enabled = GCDCore.Project.ProjectManager.Project != null;
+        }
+    }
+}
